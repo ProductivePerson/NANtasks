ac4d1dfdf78723d2c3aac92353a909cd951ca650
kaziprime@gmail.com <nazreenkazi@Nazreens-MacBook-Pro.local>
2016-07-21 16:16:19 -0400

    Fixed database.js

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,8 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var groupSchema = require('./schemas/groupSchema')
-var userSchema = require('./schemas/userSchema')
-var taskSchema = require('./schemas/taskSchema')
+var mongoose = require('mongoose'),
+    groupSchema = require('./schemas/groupSchema'),
+    userSchema = require('./schemas/userSchema'),
+    taskSchema = require('./schemas/taskSchema')
+    Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://needsclosure:needsclosure1@ds021289.mlab.com:21289/needsclosure');
 
@@ -13,58 +13,3 @@ var Group = mongoose.model('Group', groupSchema);
 
 
 module.exports = {task: Task, user: User, group: Group};
-
-// //TASK SCHEMA
-// var taskSchema = new Schema({
-//   name: String,
-//   owner: {type: Schema.Types.ObjectId, ref: 'User'},
-//   createdAt: Date,
-//   dueDate: Date,
-//   completed: Boolean,
-//   group: {type: Schema.Types.ObjectId, ref: 'Group'}
-// });
-//
-// taskSchema.pre('save', function(next){
-//   if(!this.group){this.group =  this.owner}
-//     next();
-// })
-//
-//
-//
-// //USER SCHEMA
-// var UserSchema = new Schema({
-//   username: { type: String, required: true, index: { unique: true } },
-//   password: { type: String, required: true },
-//   token: String,
-//   tasks: [{type: Schema.Types.ObjectId, ref: 'Task'}],
-//   groups: [{type: Schema.Types.ObjectId, ref: 'Group'}]
-// });
-//
-// UserSchema.pre('save', function(next) {
-//     var user = this;
-//
-// // only hash the password if it has been modified (or is new)
-// if (!user.isModified('password')) return next();
-//
-// // generate a salt
-// bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//     if (err) return next(err);
-//
-//     // hash the password using our new salt
-//     bcrypt.hash(user.password, salt, function(err, hash) {
-//         if (err) return next(err);
-//
-//         // override the cleartext password with the hashed one
-//         user.password = hash;
-//         next();
-//     });
-// });
-//
-// });
-//
-// UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//         if (err) return cb(err);
-//         cb(null, isMatch);
-//     });
-// };
