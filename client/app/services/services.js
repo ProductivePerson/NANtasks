b059263b95dbe82e60aec990aa704ecec72e772f
Tim Clark <TC.Clark25@gmail.com>
2016-07-21 10:46:33 -0400

    Add files via upload

---
+++ client/app/services/services.js
@@ -0,0 +1,217 @@
+angular.module('services', [])
+
+.factory('Tasks', function ($http) {
+
+  var isUser = function(username){
+   //console.log("I am in services", username);
+   return $http({
+     method: 'POST',
+     url: '/api/user/check',
+     data: username
+   }).then(function(resp){
+     //console.log("data from server", resp.data);
+     return resp.data;
+   })
+ };
+
+  var getUserTasks = function(user){
+      return $http({
+        method: 'POST',
+        url: '/api/user/usertasks',
+        data: user
+      }).then(function(resp){
+        //console.log(resp.data);
+        return resp.data;
+      })
+    };
+
+    var fetchAllTasks = function(){
+        return $http({
+          method: 'GET',
+          url: '/api/tasks'
+        }).then(function(resp){
+          return resp.data;
+        })
+      };
+
+  var addTask = function(task, callback){
+    console.log('working', task);
+    return $http({
+      method: 'POST',
+      url: '/api/tasks',
+      data: task
+    }).then(function(resp){
+      console.log("here is response from server", resp)
+      //using callback to update our tasks ONLY after respond
+      callback(resp);
+      //return resp;
+    })
+    .catch(function(err){
+      console.err("Error adding task: ", err);
+    })
+  };
+
+  var deleteTask = function(task, callback){
+     return $http({
+       method: 'POST',
+       url: '/api/tasks/delete',
+       data: JSON.stringify(task)
+     }).then(function(resp){
+       //using callback to update our tasks ONLY after respond
+       callback(resp);
+     }).catch(function(err){
+       console.log('Error', err);
+     })
+   };
+
+   var completeTask = function(task, callback){
+     return $http({
+       method: 'PUT',
+       url: '/api/tasks',
+       data: task
+     }).then(function(resp){
+       //using callback to update our tasks ONLY after respond
+       callback(resp);
+     }).catch(function(err){
+       console.log('Error', err);
+     })
+   };
+
+  return {
+    fetchAllTasks:fetchAllTasks,
+    getUserTasks:getUserTasks,
+    addTask: addTask,
+    deleteTask:deleteTask,
+    completeTask: completeTask,
+    isUser:isUser
+  }
+
+})
+
+.factory('Proj', function($http){
+  //function to add new project to the projects list in index.html sidebar
+  var addProject = function(project, user){
+    project.username = user;
+    return $http({
+      method: 'POST',
+      url: '/api/createGroup',
+      data: project
+    })
+    .then(function(resp){
+      //return group object sent back from server
+      return resp;
+    })
+    .catch(function(err){
+      console.error(err);
+    })
+  };
+
+  var addUserToGroup = function(user){
+    return $http({
+      method: 'PUT',
+      url: '/api/group/addUser',
+      data: user
+    })
+    .then(function(resp){
+      //return group object sent back from server
+      console.log('get response on add new user', resp)
+      return resp;
+    })
+    .catch(function(err){
+      console.error(err);
+    })
+  };
+
+  var fetchAllProjectTasks = function(id){
+    return $http({
+      method: 'POST',
+      url: "/api/group/getTasks",
+      data: {groupID: id}
+    })
+    .then(function(resp){
+      return resp;
+    })
+    .catch(function(err){
+      console.error("Error fetching all group tasks: ", err)
+    })
+  };
+  var fetchProjectMembers = function(id){
+    return $http({
+      method: 'POST',
+      url: '/api/group/getUsers',
+      data: {groupID: id}
+    })
+    .then(function(resp){
+      return resp;
+    })
+    .catch(function(err){
+      console.error(err)
+    })
+  };
+  var getUserProjectsList = function(user){
+    return $http({
+      method: "POST",
+      url: "/api/user/getGroups",
+      data: {username: user}
+    })
+    .then(function(resp){
+      return resp;
+    })
+    .catch(function(err){
+      console.error(err)
+    })
+  };
+
+  return {
+    addUserToGroup:addUserToGroup,
+    addProject: addProject,
+    fetchAllProjectTasks: fetchAllProjectTasks,
+    fetchProjectMembers: fetchProjectMembers,
+    getUserProjectsList: getUserProjectsList
+  }
+})
+
+.factory('Auth', function ($http, $location, $window) {
+  var signin = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data;
+    })
+
+  };
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data;
+    })
+  };
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.fridge');
+  };
+
+  var signout = function () {
+    $window.localStorage.removeItem('user.fridge');
+    $window.localStorage.removeItem('id.fridge');
+    $window.localStorage.removeItem('com.fridge');
+
+    $location.path('/signin');
+  };
+
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+});
---
+++ helpers.js
@@ -0,0 +1,264 @@
+var Model = require('./database.js');
+var jwt  = require('jwt-simple');
+
+
+var taskFuncs = {
+
+/* TASK FUNCTIONS */
+	
+	// checks to see if user exists if so return the userId for that user. 
+
+ checkUser: function(userName, res) {
+		Model.user.findOne({"username": userName}, function(err, found){
+		  if(err) {
+        console.log("username not found");
+		  }else {
+        res.send(found._id);
+		  }
+		})
+  },
+
+	getUserTasks: function(user, res){
+		Model.task.find({"owner": user}, function(err, tasks){
+			if(err){
+				console.log("tasks not fetched", err);
+			}
+			console.log("tasks", tasks);
+			res.send(tasks); //sends back array of tasks
+		});
+	},
+
+	addTask: function(task, res) {
+		var newTask = new Model.task(task);
+		newTask.save(function(err){
+			if(err) {
+				console.log("error:", err);
+			}
+			console.log("Task Added!", newTask);
+			res.send(newTask); //sends back added task
+		})
+	},
+
+	deleteTask: function(id, res){
+		Model.task.remove({"_id": id}, function (err) { 
+			if(err){
+				console.log("Error: ", err)
+			}
+			res.send("task removed");
+		});
+
+	},
+	completeTask: function(id, res){
+		Model.task.update({"_id": id}, {
+			completed: true
+
+		}, function(err){
+			if(err) {
+				console.log("task not marked as complete", err);
+			}
+			res.send("task marked as complete");
+		});
+	},
+
+	editTask: function(id, editedTask, res){
+		// updates the task name based on the request body and the id associated with it. 
+		
+		Model.task.update({"_id": id}, {
+			name: editedTask
+		}, function(err, obj) {
+			if(err) {
+				console.log("task update failed", err); 
+			}
+			res.send("task was updated", obj); 
+		});
+	},
+
+
+/* GROUP FUNCTIONS */
+
+//creates group and adds group to user
+createGroup: function(groupName, username, res){
+	var group = new Model.group({"name": groupName});
+
+		Model.user.update({"username": username}, {$push:{"groups": group}}, 
+			function(err){
+				if(err){
+					res.send(new Error("new group not saved to user document"))
+				}
+
+				Model.user.findOne({"username": username}, function(err, user){
+					group.users.push(user);
+					group.save(function(err){
+						if(err){res.send("group not created", err)}
+						res.send(group);
+					})
+				})
+		})
+	
+},
+
+	// adds User to Group AND adds group to user
+	addUserToGroup: function(username, groupId, res){
+		Model.user.findOne({"username": username}, function(err, user){
+			if(err){
+				res.send("User not found", err)
+			}
+
+			if(!user.length) {
+				Model.group.findOne({"_id": groupId}, function(error, group) {
+					if(error){
+						console.log("The group was not found", error); 
+					}
+					console.log("group")
+					if(group.users.indexOf(user._id) >= 0) { 
+						console.log("user already exists in group");
+						res.send(new Error("user already exists in group"));
+					}
+					else{
+						group.users.push(user); 
+						group.save(function(err){
+							console.log("Current members of group", group.users);
+							user.groups.push(group);
+							user.save(function(err){
+								res.send(group);
+							})
+						})
+					}
+				})
+			}
+			else{
+				res.send(new Error("user not found"));
+			}
+
+		})
+
+	}, 
+	
+	//get users for current group
+	getUsers: function(groupID, res){
+		Model.group.findOne({"_id": groupID}).populate('users').exec(function(err, group){
+			if(err){
+				console.log("group not found", err);
+			}
+			console.log("Members of group:", group.users);
+			res.send(group.users) //will return an array of user objects in the group
+		})
+	},
+
+	//get tasks for group
+	collectGroupTasks: function(groupId, res){
+		Model.task.find({"group": groupId}, function(error, tasks) {
+			if(error){
+				console.log("Group tasks weren't retrieved", error); 
+			}
+			console.log("successfully retrieved group tasks:", tasks);
+			res.send(tasks); 
+		});
+	},
+
+	//get groups that user is a member of
+	getGroups: function(username, res) {
+		Model.user.findOne({"username": username}).populate('groups').exec(function(error, user) {
+			if(error) {
+				console.log("Error in finding groups", error);
+			}else {
+				if(!user.groups.length) {
+					res.send("user does not have any groups");
+				}else {
+					console.log("groups: ", user.groups);
+					res.send(user.groups); 
+				}
+			}
+		})
+	}, 
+
+	deleteUserFromGroup: function(userID, groupID, res){
+		Model.group.findOne({"_id": groupID}, function(error, group){
+			group.users.remove({"_id": userID}), function(err){
+					if(err){
+						console.log("there was an error removing the user from group");
+					}
+					console.log("user removed from group"); 
+			}
+		})
+	},
+
+
+
+/* AUTHENTICATION FUNCTIONS */
+
+	signup: function(newUser, res, next) {
+		Model.user.find({"username": newUser.username}, function(err, user){
+			if(err) {
+				console.log("Error: ", error);
+			}
+			if(!user.length) { //if a user is not found, an empty array is returned
+				console.log("user does NOT already exist");
+				var user = new Model.user(newUser);
+				user.save(function(err){
+					if(err) {
+						console.log("new user not saved", err);
+					}
+				console.log("new user saved");
+				var token = jwt.encode(user, 'secret'); //create new token
+        res.json({"token": token, "user": {"id": user._id, "username": user.username}}); //send new token and user object
+			})
+
+			}
+			else {
+				console.log("user already exists: ", user);
+				next(new Error("user already exists"));
+			}
+		})
+	},
+
+	signin: function(reqUser, res, next){
+		Model.user.find({"username": reqUser.username}, function(err, user){
+			if(err){ //if error in query
+				next("Error: ", error);
+			}
+			if(!user.length){ //if user not found
+				next(new Error("username does not exist"));
+			}
+			else{ //if user found
+				user[0].comparePassword(reqUser.password, function(err, isMatch){
+					if(err) {throw err;}
+					if(!isMatch){
+						next(new Error("Incorrect password")) //will send an error if incorrect password
+					}
+					else{
+						console.log("password correct!");
+						var token = jwt.encode(user[0], 'secret'); //create new token
+            res.json({"token": token, "user": {"id": user[0]._id, "username": user[0].username}}); //send new token and user object
+					}
+				})
+			}
+		})
+	}, 
+
+	checkAuth: function(req, res, next){
+		var token = req.headers['x-access-token'];
+    if (!token) {
+      next(new Error('No token'));
+    } 
+    else {
+      var user = jwt.decode(token, 'secret');
+      console.log("Decoded user:", user);
+      Model.user.find(user, function(err, user){
+      	if(err){
+      		next("Error: ", error);
+      	}
+      	if(!user.length){ //user not found
+      		res.status(401).send();
+      	}
+      	else{ //token decoded and user found in database
+      		console.log("user authenticated")
+      		res.status(200).send();
+      	}
+      });
+    }
+	}
+}
+
+
+module.exports = taskFuncs;
