74dc88c66666a31dad6ada96fa2b028d197a6273
Tim Clark <TC.Clark25@gmail.com>
2016-07-24 17:03:54 -0400

    Implemented cat based avatar menu

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -4,8 +4,10 @@ angular.module('profile', [])
   angular.extend($scope);
   var canvas = document.getElementById('avatarCanvas');
   var brush = canvas.getContext("2d");
-  var img = loadImage("/assets/cat_1.png");
-  setTimeout(function(){
+  var img = loadImage("/assets/cat_0.png");
+  setTimeout(function() {
+    $scope.avatarNum = 0;
+    $scope.hatNum = 0;
     $scope.cavatar = img;
     $scope.showAvatar();
   }, 1000);//FIX THIS LATER;
@@ -32,34 +34,39 @@ angular.module('profile', [])
   };
 
   $scope.toggleCat = function(num) {
-
+    $scope.avatarNum = ($scope.avatarNum+num)%16;
+    $scope.cavatar.src = "/assets/cat_" + $scope.avatarNum + ".png";
+    $scope.drawHat();
   };
 
   $scope.showAvatar = function() {
-    console.log("Brush and image: ", brush, "\n", img);
-
-    brush.drawImage($scope.cavatar, 0, 0);
+    console.log("Brush and image: ", brush, "\n", $scope.img);
+    try {
+      brush.drawImage($scope.cavatar, 0, 0);
+    }
+    catch (TypeError){
+      console.log("Error doing the thing");
+    }
+    // var img = new Image();
     // var img = new Image();
     // img.src = window.location.origin + "/assets/cat_1.png";
+    // img.src = window.location.origin + "/assets/cat_1.png";
+    // brush.drawImage(img, 0, 0);
     // brush.drawImage(img, 0, 0);
   };
-
-  $scope.drawHat = function(hat) {
+  $scope.setHat = function(hat) {
+    $scope.hatNum = hat;
+    $scope.drawHat();
+  };
+  $scope.drawHat = function() {
     var catHat = document.getElementsByClassName('catHat');
-
     brush.drawImage($scope.cavatar, 0, 0);
-    for (var x = 0; x < catHat.length; x++) {
-      if (~catHat[x].src.indexOf(hat)) {
-        catHat = catHat[x];
-        break;
-      }
+    if ($scope.hatNum > 0) {
+      brush.drawImage(catHat[$scope.hatNum], 23, 10);
     }
-    console.log("Hat is ", hat);
-    brush.drawImage(catHat, 23, 10);
   };
 
   function loadImage(src) {
-      // http://www.thefutureoftheweb.com/blog/image-onload-isnt-being-called
       var img = new Image();
 
       img.onload = $scope.showAvatar;
